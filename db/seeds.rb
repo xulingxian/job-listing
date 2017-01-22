34bbdbbdbb3dbf7fc2625cce3fc35234b06782af
xulingxian <xulingxian001@gmail.com>
2017-01-22 21:22:27 +0800

    create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,30 @@
 puts "这个种子档会自动建立一个admin账号， 并且创建 10 个 public jobs, 以及10个 hidden jobs"
 
 create_account = User.create([email: 'xulingxian@qq.com', password: '620719', password_confirmation: '620719', is_admin: 'true'])
-puts "Admin account create."
+puts "Admin account is created successfully!"
+
+job_info = [
+  '招聘RoR工程师',
+  '招聘文案设计',
+  '招聘UI设计师',
+  '招聘Android开发工程师',
+  '招聘产品经理',
+  '招聘前端开发工程师',
+  '招聘市场营销',
+  '招聘php后台研发工程师',
+  '招聘python工程师',
+  '招聘高级JAVA研发工程师',
+  '招聘高级数据挖掘工程师',
+  '招聘高级客服经理'
+]
 
 create_jobs = for i in 1..10 do
 
-  Job.create!([title: "Job no.#{i}", description: "這是用種子建立的第 #{i} 個Public工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100, is_hidden: "false"])
+  Job.create!([title: job_info[rand(job_info.length)], description: "這是用種子建立的第 #{i} 個Public工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100, is_hidden: "false"])
 end
 puts "10 Public jobs created."
 
 create_jos = for i in 1..10 do
-  Job.create!([title: "Job no.#{i+10}", description: "這是用種子建立的第 #{i+10} 個Hidden工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100,is_hidden: "true"])
+  Job.create!([title: job_info[rand(job_info.length)], description: "這是用種子建立的第 #{i+10} 個Hidden工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100,is_hidden: "true"])
 end
 puts "10 Hidden jobs created."
